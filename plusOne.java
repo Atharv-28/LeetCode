5495da083f5aaf704d4e3d6b01a4a270931eb218
Atharv-28 <atharvmt@outlook.com>
2025-01-06 23:22:13 +0530

    PlusOne prob (Loop was in opposite direction)

--- plusOne.java
+++ plusOne.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=0; i<digits.length; i++) {
-            if(digits[i] != 9) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
         }
-        return new int[digits.length+1];
+        int[] newNumber = new int[digits.length + 1];
+        newNumber[0] = 1;
+        return newNumber;
     }
 }
 
 public class plusOne {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] digits = {1, 2, 3};
-        int[] newDigits = sol.plusOne(digits);
-        for(int i=0; i<newDigits.length; i++) {
-            System.out.print(newDigits[i] + " ");
+        int[] digits = { 4, 3, 2, 9 };
+        int[] result = sol.plusOne(digits);
+        for (int digit : result) {
+            System.out.print(digit + " ");
         }
     }
 }
