c4063aa32d61548a4938d1f98b618411caa01776
Atharv Mayur Tambekar <110536276+Atharv-28@users.noreply.github.com>
2026-09-07 16:54:21 +0530

    Time: 1 ms (99.94%), Space: 47 MB (45.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Boolean> map= new HashMap<>();
-
+        int xor=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(map.containsKey(nums[i]))
-            {
-                map.put(nums[i], false);
-            }
-            else{
-                map.put(nums[i], true);
-            }
+            xor ^= nums[i];
         }
-        Integer trueKey = null;
 
-        for (Map.Entry<Integer, Boolean> entry : map.entrySet()) {
-            if (Boolean.TRUE.equals(entry.getValue())) {
-                trueKey = entry.getKey();
-                break; 
-            }
-        }   
-        return trueKey;
+        return xor;
     }
 }
